830c729a597a88620f66ebb842fcce1ba006346d
Fernando Gordillo <fermg299@outlook.com>
2024-12-05 12:35:08 -0300

    Storybook listo

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,60 @@
+import './MyLabel.css'
+
+/**
+ * Description placeholder
+ *
+ * @interface Props
+ * @typedef {Props}
+ */
+/**
+ * Description placeholder
+ *
+ * @interface Props
+ * @typedef {Props}
+ */
+interface Props {
+  /**
+   * Text to display
+   */
+  label: string
+
+  /**
+   * Label size
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3'
+
+  /**
+   * Capitalize all letters
+   */
+  allCaps?: boolean
+
+  /**
+   * Label color
+   */
+  color?: 'text-primary' | 'text-secondary' | 'text-terciary'
+
+  /**
+   * Font Color
+   */
+  fontColor?: string
+
+  
+}
+
+
+
+export const MyLabel = ({ 
+  label, 
+  size = 'normal',
+  allCaps = false,
+  color,
+  fontColor
+} : Props) => {
+  return (
+    <span className={`${ size } ${ color }`}
+      style={{ color: fontColor }}
+    >
+      { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
